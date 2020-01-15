26f697ff53eaa5e1eb46ff403c920d379c0580bd
Robert Cox <rcox18@mail.greenriver.edu>
2020-01-15 14:17:21 -0500

    added index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author     Robert Cox
+ * @version    1.0.0
+ * @link       http://rcox.greenriverdev.com/IT328/chicken
+ * @since      1/15/2020
+ */
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
